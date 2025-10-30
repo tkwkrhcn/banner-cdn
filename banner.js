c770ee3f9e831143589ea39736a8024f29f88ae2
tkwkrhcn <tkwkrhcn11@gmail.com>
2025-10-30 16:41:11 +0900

    Update banner.js

--- banner.js
+++ banner.js
@@ -38,13 +38,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // 2. 배너 데이터
   const fixedBanners = [
-    { url: "https://xn--vy7ba476b.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/c4352a78-3a5f-42ce-5255-5f9be9ced200/public" },
-    { url: "https://xn--p49al7tolbl8o8tj.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/ff2d6728-7523-4452-bd48-ecdb8072a600/public" }
+    { url: "https://xn--vy7ba476b.com/", img: "https://imgur.com/dTUnYMz.gif" },
+    { url: "https://xn--p49al7tolbl8o8tj.com/", img: "https://imgur.com/X9UgP7y.png" }
   ];
 
   const banners = [
-    { url: "https://m-opgo.com/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/42adcbce-2231-4973-8e14-0ee48c8b3f00/public" },
-    { url: "https://aha-o300.com/", img: "https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/ef33775e-5535-44d3-7638-9e6a0eaf6b00/public" },
+    { url: "https://m-opgo.com/", img: "https://imgur.com/FMHlKQh.gif" },
+    { url: "https://aha-o300.com/", img: "https://imgur.com/U6BsQbf.gif" },
     { url: "https://jp12t4.com/", img: "https://imgur.com/SWjdiLE.gif" },
     { url: "https://opview76.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/f035c716-5ed6-4d68-2896-fe07c463c300/public" },
     { url: "https://gt206.com/", img: "https://imgur.com/h8ZScoC.gif" },
