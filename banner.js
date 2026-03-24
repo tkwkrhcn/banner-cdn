377483a121896bf2e9dcd16e23932252e14c8ce7
tkwkrhcn <tkwkrhcn11@gmail.com>
2026-03-24 12:16:17 +0900

    Replace URL and image in banner.js

--- banner.js
+++ banner.js
@@ -55,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
     { url: "https://sv-vvv.com/", img: "https://soranet3.com/da/01KJ4DJ73EJMQH1165KS02MZXD.gif" },
     { url: "https://sm-mmm.com/", img: "https://soranet3.com/da/01KJ4DKFP278CFE9EPEJGYBZN7.gif" },
     { url: "http://mco-ccc.com/", img: "https://soranet3.com/da/01KJ4DM5516R1TKGD0X978RW0N.gif" },
-    { url: "http://spst-ddd.com/", img: "https://soranet3.com/da/01KJ4DMZ72T65Y311XEQ590MV6.gif" },
+    { url: "http://pmu777.com/", img: "https://soranet3.com/da/01KMEX9AGS8SXNACZH0G7G2ZPA.gif" },
     { url: "https://mnb-bbb.com/", img: "https://soranet3.com/da/01KJ4DP2MTX403E6EQEHX8MDHY.gif" }
   ];
 
